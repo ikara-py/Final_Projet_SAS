80c5108b6992382f20d4b9adf04cf62140cdb145
ali kara <ali16kara@gmail.com>
2025-09-11 21:52:26 +0100

    split the files

---
+++ airport.h
@@ -0,0 +1,57 @@
+#ifndef AIRPORT_H
+# define AIRPORT_H
+
+#include <stdio.h>
+#include <string.h>
+#include "airport.h"
+
+
+#define MAX_AIRPLANES 200
+#define inner_string_size 50
+
+void Menu();
+void UserChoice(int choice);
+void AirplaneMenu();
+void addAirplane();
+void viewAirplanes();
+void deletAirplanes();
+void update_Airplane();
+void update_statut();
+void update_capacity();
+void aviability();
+void update_modele();
+void statistics();
+void sort();
+double Fleet_Occupancy();
+void airPort();
+void searchAirplane();
+
+typedef struct {
+    int day;
+    int month;
+    int year;
+} date;
+
+typedef struct {
+    date entryDate;
+    char modele[50];
+    int id;
+    int capacite;
+    char statut[20];
+} airplanes;
+
+typedef struct {
+    char name[100];
+    int max_capacity;
+    int count;
+    int total_airplanes;
+    int aviable;
+    int under_maintenance;
+    int in_flight;
+    int id_counter;
+    airplanes newairplan[MAX_AIRPLANES];
+} airport;
+airport newAirport;
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,24 @@
+#include "airport.h"
+#include <stdio.h>
+
+int main() {
+    newAirport.count = 0;
+    newAirport.total_airplanes = 0;
+    newAirport.aviable = 0;
+    newAirport.under_maintenance = 0;
+    newAirport.in_flight = 0;
+    newAirport.id_counter = 0;
+    strcpy(newAirport.name, "Default Airport");
+    newAirport.max_capacity = MAX_AIRPLANES;
+    int choice;
+    do {
+        Menu();
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        while (getchar() != '\n');
+        UserChoice(choice);
+    } while (choice != 0);
+
+    return 0;
+}
+
